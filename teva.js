b157efe36bb3ea8584b91bf937e918700a80b848
menviv <menashe.gezelkopy@gmail.com>
2017-03-15 13:49:43 +0200

    test bs

--- teva.js
+++ teva.js
@@ -204,7 +204,7 @@ $( "#ButtonQ1BS" ).click(function() {
   
   if (BsAn2 == "True") {
   
-  		$("FADEOUT").fadeOut( 500 );
+  		$("FADEOUT").fadeOut(3000);
       
       $("#rightbs").fadeIn( 500 );
       $("#RightRadioButton").addClass("right:input[type='radio']:checked:after");
