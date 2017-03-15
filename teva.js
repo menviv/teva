4126ac30a5eadf44f967de87fee02353e34e7df4
menviv <menashe.gezelkopy@gmail.com>
2017-03-15 11:11:12 +0200

    Update teva.js

--- teva.js
+++ teva.js
@@ -1608,12 +1608,6 @@ $( document ).ready(function() {
     
    } 
   
- 
-
-  
-   if (PT_done = "Done" && KFARSABA_done = "Done" && SHMONA_done = "Done" && NATANYA_done = "Done" && ASHDOD_done = "Done" && REHOVOT_done = "Done" && SHOHAM_done = "Done" && JERUSALEM_done = "Done" && BS_done = "Done" {  
-    
-       $( "#ENDOFGAME" ).fadeIn( 500 );
     
     
    });
